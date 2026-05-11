803c445e5694c03e038e5e5787c529ffaeb9256d
thierryhanry2006 <thierrycardoso7@gmail.com>
2026-05-11 13:49:22 -0300

    Atualização cobrança, plano, app e login

--- crm-planos-saude/app.py
+++ crm-planos-saude/app.py
@@ -13,11 +13,7 @@ def home():
     clientes = listar_todos_clientes()
     alertas = listar_alertas_pendentes()
     total_hoje = contar_alertas_hoje()
-    
-    return render_template("Plano.html", 
-                           clientes=clientes, 
-                           alertas=alertas, 
-                           total_hoje=total_hoje)
+    return render_template("Plano.html", clientes=clientes, alertas=alertas, total_hoje=total_hoje)
 
 @app.route("/clientes", methods=["POST"])
 def cadastrar_cliente():
@@ -47,4 +43,4 @@ def deletar_cliente(id):
         return f"Erro ao excluir cliente: {e}"
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
